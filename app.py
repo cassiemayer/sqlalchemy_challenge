ee21b2ee56e37cbb6403eda3bd061fada0e01a2b
cassiemayer <cassiemayer@yahoo.com>
2023-05-25 19:17:26 -0400

    push to github

--- app.py
+++ app.py
@@ -7,3 +7,28 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+# Database Setup 
+
+engine = create_engine("sql:///hawaii.sqlite")
+
+# Reflect an exisiting database into a new model
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+#Save reference to table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#create session
+session = Session(engine)
+
+#Flask setup
+app = Flask(__name__)
+
+#Flask route
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return(
+        f""
+        
+    )
